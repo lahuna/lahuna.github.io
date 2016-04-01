a3e7130d1f5990d4823b032d11d3eb501bf40133
chris <genxstocks@yahoo.com>
2016-04-01 01:24:27 -0700

    order store

---
+++ js/controllers/modal.js
@@ -0,0 +1,20 @@
+//*****************************************************************************************************************
+// Copyright � 2014 - 2016 Lahuna. All rights reserved.
+// You may not copy, reproduce, republish, disassemble, decompile, reverse engineer, post, broadcast, transmit, or
+// make available to the public any content or code on this website without prior written permission from Lahuna.
+//*****************************************************************************************************************
+
+'use strict';
+
+var ctl = angular.module('ModalInstanceController', []);
+
+ctl.controller('ModalInstanceCtrl', function ($scope, $modalInstance) {
+
+    $scope.ok = function () {
+        $modalInstance.close('ok');
+    };
+
+    $scope.cancel = function () {
+        $modalInstance.dismiss('cancel');
+    };
+});
