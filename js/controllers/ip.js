dab62b521c8f4cc1fdf166782643334a9826a24a
chris <genxstocks@yahoo.com>
2016-10-19 21:27:38 -0700

    cleanup

---
+++ js/controllers/ip.js
@@ -0,0 +1,33 @@
+//*****************************************************************************************************************
+// Copyright � 2016 Feed. All rights reserved.
+// You may not copy, reproduce, republish, disassemble, decompile, reverse engineer, post, broadcast, transmit, or
+// make available to the public any content or code on this website without prior written permission from Feed.
+//*****************************************************************************************************************
+
+'use strict';
+
+var ctl = angular.module('IpController', ['Resources']);
+
+ctl.controller('IpCtrl', function ($scope, $routeParams, $location, LogResource) {
+  var ip = $routeParams.ip;
+  $scope.ip = ip;
+  $scope.log = LogResource.Get({
+    'type': 'ip',
+    'ipAddress': ip
+  });
+
+  $scope.delete = function(index, id) {
+    $scope.log.list.splice(index, 1);
+    LogResource.Delete({
+      '_id': id,
+      'type': 'ip'
+    });
+  }
+
+  $scope.deleteAll = function() {
+    $scope.log = undefined;
+    LogResource.Delete({
+      'type': 'ip'
+    });
+  }
+});
---
+++ js/controllers/log.js
@@ -0,0 +1,33 @@
+//*****************************************************************************************************************
+// Copyright � 2016 Feed. All rights reserved.
+// You may not copy, reproduce, republish, disassemble, decompile, reverse engineer, post, broadcast, transmit, or
+// make available to the public any content or code on this website without prior written permission from Feed.
+//*****************************************************************************************************************
+
+'use strict';
+
+var ctl = angular.module('LogController', ['Resources']);
+
+ctl.controller('LogCtrl', function ($scope, $routeParams, $location, LogResource) {
+  var ip = $routeParams.ip;
+  $scope.ip = ip;
+  $scope.log = LogResource.Get({
+    'type': 'log',
+    'ip': ip
+  });
+
+  $scope.delete = function(index, id) {
+    $scope.log.list.splice(index, 1);
+    LogResource.Delete({
+      '_id': id,
+      'type': 'log'
+    });
+  }
+
+  $scope.deleteAll = function() {
+    $scope.log = undefined;
+    LogResource.Delete({
+      'type': 'log'
+    });
+  }
+});
